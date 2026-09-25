e7ba021a91c7f376d71e853f022a30ff31969d8c
analan2 <analaulan2@gmail.com>
2026-09-25 02:35:40 -0300

    intenté hacer una plataforma móvil
    pero tengo mucho sueño, llegué hasta la mitad y la plataforma no se detiene, capaz le faltan colliders

---
+++ Assets/PlataformaMovil.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Numerics;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+
+public class PlataformaMovil : MonoBehaviour
+
+{
+    // creo que es para elegir la plataforma
+    public GameObject platform;
+
+    // y ahora para tener parámetro de velocidad:
+    public float moveSpeed;
+
+    public Transform currentPoint; 
+
+    public Transform[] points;
+
+    public int pointSelection;
+
+    void Start ()
+    {
+        currentPoint = points[pointSelection];
+    }
+
+    void Update ()
+    {
+        platform.transform.position = Vector3.MoveTowards(platform.transform.position, currentPoint.position, Time.deltaTime * moveSpeed);
+    }
+}
